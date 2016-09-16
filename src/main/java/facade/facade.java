d1c3ff9629fd536ee4b06b2c6ecc473e7e7ee401
matyasfeher <fehermatyas17@gmail.com>
2016-09-16 15:11:08 +0200

    1

---
+++ src/main/java/facade/facade.java
@@ -0,0 +1,33 @@
+package facade;
+
+/**
+ *
+ * @author Acer
+ */
+public class facade {
+    
+    public void createEmployee(){
+    
+        
+    }
+    
+    public void updateCustomer(){
+    
+    }
+    
+    public void getEmployeeCount(){
+    
+    }
+    public void getCustomerInCity(String city){
+    
+    }
+    public void getEmployeeMaxCustomers(){
+    
+    }
+    public void getOrdersOnHold(){
+    
+    }
+    public void getOrderOnHold(int customerNumber){
+    
+    }
+}
